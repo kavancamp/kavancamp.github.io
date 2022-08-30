61857e05594739c0ffc6b9d1f376501a8d0b7d30
Keenah <kvancamp@mail.com>
2022-08-29 20:26:59 -0500

    removed unnecessary text

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-
-import './App.css';
+import "./App.css";
+import React from 'react';
+import { About, Contact, Footer, Profile, Resume, Testimonial } from './container';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <Profile />
+     
     </div>
   );
 }
