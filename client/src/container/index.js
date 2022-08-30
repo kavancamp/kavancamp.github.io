417ac9b1be48b2be2d91195fbfc754cd490fe666
Keenah <kvancamp@mail.com>
2022-08-29 20:27:19 -0500

    exported container modules

---
+++ client/src/container/index.js
@@ -0,0 +1,8 @@
+export { default as About } from "./About/About";
+export { default as Contact } from "./Contact/Contact";
+export { default as Profile } from "./Home/Profile";
+export { default as Footer } from "./Footer/Footer";
+export { default as Resume } from "./Resume/Resume";
+export { default as Testimonial } from "./Testimonial/Testimonial";
+
+
