b3ef357b7ee8e6412a5a33e5d99158c515dd8141
Keenah <kvancamp@mail.com>
2022-08-31 14:46:41 -0500

    added images

--- client/src/container/Home/Profile.js
+++ client/src/container/Home/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Profile.css";
-import Typical from 'react-typical'
+import Typical from "react-typical";
 
 function Profile() {
   return (
@@ -8,15 +8,17 @@ function Profile() {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a href="https://www.linkedin.com/in/keenah-vancampenhout/">
-              <i className="fa fa-linkedin-square"></i>
-            </a>
-            <a href="https://github.com/kavancamp">
-              <i className="fa fa-github-square"></i>
-            </a>
-            <a href="https://www.facebook.com/keenah.vancampenhout/">
-              <i className="fa fa-facebook-square"></i>
-            </a>
+            <div className="colz-icon">
+              <a href="https://www.linkedin.com/in/keenah-vancampenhout/">
+                <i className="fa fa-linkedin-square"></i>
+              </a>
+              <a href="https://github.com/kavancamp">
+                <i className="fa fa-github-square"></i>
+              </a>
+              <a href="https://www.facebook.com/keenah.vancampenhout/">
+                <i className="fa fa-facebook-square"></i>
+              </a>
+            </div>
           </div>
 
           <div className="profile-details-name">
@@ -26,9 +28,37 @@ function Profile() {
           </div>
           <div className="profile-details-role">
             <span className="primary-text">
-              <h1></h1>
+              <h1>
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Engthusiastic Dev 🧙🏻‍♀️",
+                    1000,
+                    "Full Stack Developer 🤖",
+                    1000,
+                    "MERN Stack Dev 👩🏼‍💻",
+                    1000,
+                    "React/React Native Dev 🫀",
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Knack of building applications with front and back end blahblah
+                lorem ipsum
+              </span>
             </span>
           </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">{""} Hire me </button>
+            <a href="Profile (4).pdf" download="Profile (4).pdf">
+              <button className="btn highlighted-btn">Download Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <img src="../../assets/IMG_2701.JPG" alt="Keenah" />
+          <div className="profile-picture-background"></div>
         </div>
       </div>
     </div>
