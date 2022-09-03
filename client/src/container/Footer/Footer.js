7a5e0abc05322b8594c9f3ea224e693e10879c8c
Keenah <kvancamp@mail.com>
2022-09-03 15:04:53 -0500

    Update Footer.js

--- client/src/container/Footer/Footer.js
+++ client/src/container/Footer/Footer.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./Footer.css";
-
-function Footer() {
-  return <div>Footer</div>;
+import ScrollService from "../../utilities/ScrollService";
+export default function Footer() {
+  return (
+    <div className="scroll-container">
+      <button
+        className="btn-scroll"
+        onClick={() => ScrollService.scrollHandler.scrollToHome()}
+      >
+        {" "}
+        <i className="fa fa-arrow-up"></i>
+      </button>
+    </div>
+  );
 }
-
-export default Footer;
