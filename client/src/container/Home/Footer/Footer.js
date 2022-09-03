83ec3203a599445483915ac6b774a8c1f9b6e082
Keenah <kvancamp@mail.com>
2022-09-03 15:04:58 -0500

    Create Footer.js

---
+++ client/src/container/Home/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="footer-parent">
+        <img src={require("../../../assets/shape-bg.png")} alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
