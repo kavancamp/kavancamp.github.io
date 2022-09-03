ceefcbd68def0aac93cc628820764fb8fbf7e136
Keenah <kvancamp@mail.com>
2022-09-03 15:04:42 -0500

    Update Contact.js

--- client/src/container/Contact/Contact.js
+++ client/src/container/Contact/Contact.js
@@ -1,8 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
+import Typical from "react-typical";
+import axios from "axios";
+import { toast } from "react-toastify";
+import load1 from "../../../src/assets/loading.gif";
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
+import Footer from "../../container/Footer/Footer";
 import "./Contact.css";
 
-function Contact() {
-  return <div>Contact</div>;
-}
+export default function Contact(props) {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
 
-export default Contact;
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [banner, setBanner] = useState("");
+  const [bool, setBool] = useState(false);
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleMessage = (e) => {
+    setMessage(e.target.value);
+  };
+  console.log(name);
+  const submitForm = async (e) => {
+    e.preventDefault();
+    try {
+      let data = {
+        name,
+        email,
+        message,
+      };
+      setBool(true);
+      const res = await axios.post(`/contact`, data);
+      if (name.length === 0 || email.length === 0 || message.length === 0) {
+        setBanner(res.data.msg);
+        toast.error(res.data.msg);
+        setBool(false);
+      } else if (res.status === 200) {
+        setBanner(res.data.msg);
+        toast.success(res.data.msg);
+        setBool(false);
+
+        setName("");
+        setEmail("");
+        setMessage("");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="main-container fade-in" id={props.id || ""}>
+      <ScreenHeading subHeading={"Lets Keep In Touch"} title={"Contact Me"} />
+      <div className="central-form">
+        <div className="col">
+          <h2 className="title">
+            <Typical loop={Infinity} steps={["Get In Touch 📧", 1000]} />
+          </h2>{" "}
+          <a href="https://www.linkedin.com/in/keenah-vancampenhout/">
+            <i className="fa fa-linkedin-square"></i>
+          </a>
+          <a href="https://github.com/kavancamp">
+            <i className="fa fa-github-square"></i>
+          </a>
+          <a href="https://www.facebook.com/keenah.vancampenhout/">
+            <i className="fa fa-facebook-square"></i>
+          </a>
+        </div>
+        <div className="back-form">
+          <form onSubmit={submitForm}>
+            <p>{banner}</p>
+            <label htmlFor="name">Name</label>
+            <input type="text" onChange={handleName} value={name} />
+
+            <label htmlFor="email">Email</label>
+            <input type="email" onChange={handleEmail} value={email} />
+
+            <label htmlFor="message">Message</label>
+            <textarea type="text" onChange={handleMessage} value={message} />
+
+            <div className="send-btn">
+              <button type="submit">
+                send
+                <i className="fa fa-paper-plane" />
+                {bool ? (
+                  <b className="load">
+                    <img src={load1} alt="not responding" />
+                  </b>
+                ) : (
+                  ""
+                )}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
