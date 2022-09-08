1cc5c06d78a63024c09868f2e0b150652ab50694
Keenah <kvancamp@mail.com>
2022-09-08 18:43:14 -0500

    Update Contact.js

--- client/src/container/Contact/Contact.js
+++ client/src/container/Contact/Contact.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Typical from "react-typical";
 import axios from "axios";
 import { toast } from "react-toastify";
-import load1 from "../../../src/assets/loading.gif";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../utilities/ScrollService";
 import Animations from "../../utilities/Animations";
 import Footer from "../../container/Footer/Footer";
 import "./Contact.css";
+import emailjs from "emailjs-com"
 
 export default function Contact(props) {
   let fadeInScreenHandler = (screen) => {
@@ -33,34 +33,27 @@ export default function Contact(props) {
   const handleMessage = (e) => {
     setMessage(e.target.value);
   };
-  console.log(name);
-  const submitForm = async (e) => {
-    e.preventDefault();
-    try {
-      let data = {
-        name,
-        email,
-        message,
-      };
-      setBool(true);
-      const res = await axios.post(`/contact`, data);
-      if (name.length === 0 || email.length === 0 || message.length === 0) {
-        setBanner(res.data.msg);
-        toast.error(res.data.msg);
-        setBool(false);
-      } else if (res.status === 200) {
-        setBanner(res.data.msg);
-        toast.success(res.data.msg);
-        setBool(false);
+ 
+  const form = useRef();
 
-        setName("");
-        setEmail("");
-        setMessage("");
-      }
-    } catch (error) {
-      console.log(error);
+    const submitForm = (e) => {
+      let data = {name, email, message}
+      setBool(true);
+      e.preventDefault();
+    emailjs.sendForm('service_mryay1t','template_dej4dth', '#contact-form','kZ8TxUASbtp2iM2Wc')
+      .then(function(response) {
+        setBanner(response.data);
+        toast.success(response.data);
+        setBool(false);
+        setName('');
+        setEmail('');
+        setMessage('');
+        console.log('SUCCESS!', response.status, response.text);
+      },
+      function(error) {
+        console.log('FAILED...', error);
+      });
     }
-  };
 
   return (
     <div className="main-container fade-in" id={props.id || ""}>
@@ -81,28 +74,21 @@ export default function Contact(props) {
           </a>
         </div>
         <div className="back-form">
-          <form onSubmit={submitForm}>
+          <form id="contact-form" ref={form} onSubmit={submitForm}>
             <p>{banner}</p>
-            <label htmlFor="name">Name</label>
-            <input type="text" onChange={handleName} value={name} />
+            <label >Name</label>
+            <input name="name" id="name" type="text" onChange={handleName} value={name}/>
 
-            <label htmlFor="email">Email</label>
-            <input type="email" onChange={handleEmail} value={email} />
+            <label  >Email</label>
+            <input name="email" id="email" type="email" onChange={handleEmail} value={email} />
 
-            <label htmlFor="message">Message</label>
-            <textarea type="text" onChange={handleMessage} value={message} />
+            <label >Message</label>
+            <textarea name="message" id="message"type="text" onChange={handleMessage} value={message}  />
 
             <div className="send-btn">
               <button type="submit">
                 send
                 <i className="fa fa-paper-plane" />
-                {bool ? (
-                  <b className="load">
-                    <img src={load1} alt="not responding" />
-                  </b>
-                ) : (
-                  ""
-                )}
               </button>
             </div>
           </form>
